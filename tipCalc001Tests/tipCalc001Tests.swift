4eeae0678ba202de1fbb0316fd052831fbe73718
evx001 <eviexeris@gmail.com>
2014-10-07 01:11:39 -0400

    init

---
+++ tipCalc001Tests/tipCalc001Tests.swift
@@ -0,0 +1,36 @@
+//
+//  tipCalc001Tests.swift
+//  tipCalc001Tests
+//
+//  Created by evx on 10/7/14.
+//  Copyright (c) 2014 evxyz001. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class tipCalc001Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
